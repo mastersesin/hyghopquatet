c4cc0882a40d0b89939456806116464c7a47d7d7
Ty Tran <ty.tt@softworld.jp>
2019-12-06 22:42:45 +0700

    Done BE

--- Src/View/View.py
+++ Src/View/View.py
@@ -13,17 +13,19 @@ def index():
 @transaction.route('/api/v1/order', methods=['POST'])
 def order():
     if request.is_json:
-        name = request.json.get('name', None)
-        congty = request.json.get('congty', None)
-        email = request.json.get('email', None)
-        dienthoai = request.json.get('dienthoai', None)
-        diachi = request.json.get('diachi', None)
-        soluong = request.json.get('soluong', None)
-        ngaycanhang = request.json.get('ngaycanhang', None)
-        ghichukhac = request.json.get('ghichukhac', None)
-        params = [name, congty, email, dienthoai, diachi, soluong, ngaycanhang, ghichukhac]
+        # name = request.json.get('name', None)
+        # congty = request.json.get('congty', None)
+        # email = request.json.get('email', None)
+        # dienthoai = request.json.get('dienthoai', None)
+        # diachi = request.json.get('diachi', None)
+        # soluong = request.json.get('soluong', None)
+        # ngaycanhang = request.json.get('ngaycanhang', None)
+        # ghichukhac = request.json.get('ghichukhac', None)
+        # params = [name, congty, email, dienthoai, diachi, soluong, ngaycanhang, ghichukhac]
+        params = request.json.get('params', None)
         if params:
-            return jsonify(Order.view(params=params))
+            Order.view(params=params)
+            return jsonify({'code': 1, 'msg': 'Đặt hàng thành công'})
         else:
             return jsonify({'code': 0, 'msg': 'Invalid request.'})
     else:
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
 from Src import app
 
-app.run()
+if __name__ == "__main__":
+    app.run()
